3d4bd8000c2ddcad25d4996f8c436d29f732063f
tatikaguu <tatilee100@gmail.com>
2025-10-31 09:22:03 +1030

    plots updated

--- final/code/algorithms/GSEMO.py
+++ final/code/algorithms/GSEMO.py
@@ -10,13 +10,10 @@ import os
 Implementing the GSEMO algorithm to find the optimum solution of some predefined (bi-objective 
 maximisation) problem 'func'. 
 
-The algorithm uses a multi-objective fitness function as defined below for evaluation, and is 
-uniformly constrained through parameter k. 
+The algorithm uses a multi-objective fitness function as defined below for evaluation. 
 '''
 
 class GSEMO(Algorithm):
-
-    run_counter = 0
 
     def __init__(self, budget: int):
         super().__init__(budget, name="GSEMO", algorithm_info="GSEMO algorithm implemented for maximisation with a uniform constraint.")
@@ -29,18 +26,16 @@ class GSEMO(Algorithm):
         The function returns how x dominates y for some problem predefined func. 
         '''
 
-        if (x_fitn >= y_fitn and np.sum(x) <= np.sum(y)):
-            return "WEAK"
         if (x_fitn > y_fitn and np.sum(x) < np.sum(y)):
             return "STRICT"
+        if (x_fitn >= y_fitn and np.sum(x) <= np.sum(y)):
+            return "WEAK"
         if (x_fitn < y_fitn and np.sum(x) > np.sum(y)):
             return "DOMINATED"
         else:
             return "INCOMPARABLE"
 
     def __call__(self, func: ioh.problem.GraphProblem):
-
-        self.__class__.run_counter += 1
 
         n = func.meta_data.n_variables
         mut_rate = 1/n 
@@ -62,9 +57,25 @@ class GSEMO(Algorithm):
                     x_mut[j] = 1 - x_mut[j]
 
             f_mut = func(x_mut)
-            
+
+
+            # Optimisation step: 
+            # Ensure fitness is positive; if not, repair by flipping one bits
+            ''' 
+            if (f_mut < 0): 
+                Option to repair solution by flipping 1 bits 
+                one_indices = np.where(x_mut == 1)[0]
+                for i in one_indices:
+                    x_mut[i] = 0
+                    f_new = func(x_mut)
+                    if f_new >= 0:
+                        f_mut = f_new
+                        break    
             '''
+            
+            # Optimisation step: 
             # Enforce uniform constraint (feasibility for maximum number of 1-bits k) by repairing individuals
+            '''
             while (np.sum(x_mut) > constraint_k):
                 one_indices = np.where(x_mut == 1)[0]
                 x_mut[np.random.choice(one_indices)] = 0
@@ -78,10 +89,15 @@ class GSEMO(Algorithm):
                 # Delete all solutions that x weakly dominates
                 pareto = [(y, y_fit) for y, y_fit in pareto if self.multObjFit(x_mut, f_mut, y, y_fit) != "WEAK"]  
                 
+                '''
+                Alternative deletion that optimises
+                pareto = [(y, y_fit) for y, y_fit in pareto if self.multObjFit(x_mut, f_mut, y, y_fit) != "STRICT"]  
+                '''
+
                 # Add to indivudual pareto set
                 pareto.append((x_mut, f_mut))    
 
-
+        
         # PLOTTING TRADE-OFFS BETWEEN OBJECTIVES: f(x) vs. number of 1-bit elements (as constrained by k)
         dir = "trade-off plots"
         os.makedirs(dir, exist_ok=True)
@@ -102,5 +118,5 @@ class GSEMO(Algorithm):
             plot.title(f"GSEMO Trade-Offs for F{func.meta_data.problem_id}")
             plot.savefig(out_path)
             plot.close()
+        
 
-            
--- final/code/utilities/config.py
+++ final/code/utilities/config.py
@@ -7,20 +7,20 @@ import ioh
 BUDGET = 10000   # maximum number of function evaluations per run (or number of iterations or generations for GAs)
 DIMENSION = 450   # problem dimension/size (e.g., number of bits for OneMax and LeadingOnes)
 # REPETITIONS = 10  # number of independent repetitions or runs for each problem
-REPETITIONS = 1  # number of independent repetitions or runs for each problem
+REPETITIONS = 30  # number of independent repetitions or runs for each problem
 
 PROBLEM_IDS = [ # list of problems, identified by the following IDs, to be run in our experiment in `main.py`
-    # 2100, # MaxCoverage Problem
-    # 2101,
-    # 2102,
-    # 2103,
-    # 2200, # MaxInfluence Problem
-    # 2201,
-    # 2202,
+    2100, # MaxCoverage Problem
+    2101,
+    2102,
+    2103,
+    2200, # MaxInfluence Problem
+    2201,
+    2202,
     2203,
-    # 2300, # PackWhileTravel Problem
-    # 2301,
-    # 2302
+    2300, # PackWhileTravel Problem
+    2301,
+    2302
 ]
 # PROBLEMS_TYPE = ioh.ProblemClass.PBO  # Pseudo-Boolean Optimization problems
 PROBLEMS_TYPE = ioh.ProblemClass.GRAPH  # Graph problems
@@ -38,5 +38,5 @@ ALGORITHMS = [
     # RandomizedLocalSearch(budget=BUDGET),
     # DesignedGA(budget=BUDGET, population_size=44, mutation_rate=0.01),
     # ACO(budget=BUDGET)
-    GSEMO(budget=BUDGET)
+    GSEMO(budget=BUDGET) 
 ]
